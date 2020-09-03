e50eacd04ef9bf7ba0607c39171578e9fe308a24
daisukenikadori <d.nikadori@gmail.com>
2020-09-04 02:23:55 +0900

    add put method

--- practice_fastapi/main.py
+++ practice_fastapi/main.py
@@ -1,6 +1,13 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+  name: str
+  price: float
+  is_offer: Optional[bool] = None
 
 @app.get("/")
 def read_root():
@@ -9,4 +16,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def item_id(item_id: int, q: str = None):
   return {"item_id": item_id, "q": q}
-  
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+  return {"item_name": item.name, "item_id": item_id}
