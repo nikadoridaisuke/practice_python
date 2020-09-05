8f42c21aa0397cc74a74ff55805a0acd59e70685
daisukenikadori <d.nikadori@gmail.com>
2020-09-06 01:49:44 +0900

    Path Parameters

--- tutorial_fastapi/main.py
+++ tutorial_fastapi/main.py
@@ -1,7 +1,40 @@
+from enum import Enum
 from fastapi import FastAPI
 
-my_awesome_api = FastAPI()
+class ModelName(str, Enum):
+  alexnet = "alexnet"
+  resnet = "resnet"
+  lenet = "lenet"
 
-@my_awesome_api.get("/")
+app = FastAPI()
+
+@app.get("/")
 async def root():
   return {"Message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+  return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+  return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+  return {"user_id": user_id}
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+  if model_name == ModelName.alexnet:
+    return {"model_name": model_name, "message": "Deep Learning FTW!"}
+  
+  if model_name.value == "lenet":
+    return {"model_name": model_name, "message": "LeCNN all the images"}
+
+  return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+  return {"file_path": file_path}
+
