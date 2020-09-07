a03850348c9bfdc2c3272a766e8f07286762a53f
daisukenikadori <d.nikadori@gmail.com>
2020-09-07 22:23:27 +0900

    Query Parameters

--- tutorial_fastapi/main.py
+++ tutorial_fastapi/main.py
@@ -8,9 +8,15 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 @app.get("/")
 async def root():
   return {"Message": "Hello World"}
+
+@app.get("/items/")
+async def read_item(skip: int= 0, limit: int = 10):
+  return fake_items_db[skip : skip + limit]
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
